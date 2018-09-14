1d58e1b2732bac313fc193796d94169f2e6fb25e
Bill Shoener <nitrocipher@gmail.com>
2018-09-14 17:06:31 -0500

    Update ZenithViewer.user.js

--- ZenithViewer.user.js
+++ ZenithViewer.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Zenith Viewer
 // @namespace    http://tampermonkey.net/
-// @version      1.2b
+// @version      1.2c
 // @description  try to take over the world!
 // @require      https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js
 // @require      https://timeago.yarp.com/jquery.timeago.js
@@ -20,7 +20,21 @@
     var parsedURL;
     var relatedLinks = "<div #upnext style='font-size: 16px;'>Related:</div><br/>";
 
-    setTimeout(function(){ replaceVideo(); }, 3000);
+    var waitForEl = function(selector, callback) {
+        if (jQuery(selector).length) {
+            callback();
+        } else {
+            setTimeout(function() {
+                waitForEl(selector, callback);
+            }, 100);
+        }
+    };
+
+    waitForEl("yt-player-error-message-renderer", function() {
+        replaceVideo();
+    });
+
+    //setTimeout(function(){ replaceVideo(); }, 3000);
 
     function replaceVideo() {
         if ($("yt-player-error-message-renderer")[0]){
@@ -30,7 +44,7 @@
                 //alert(parsedURL);
                 $("yt-player-error-message-renderer").replaceWith(`<video autoplay width="`+document.getElementById("primary-inner").offsetWidth+`" controls><source src="` +parsedURL+ `" type="video/mp4"></video>`);
             });
-            var relatedAPI = "https://www.googleapis.com/youtube/v3/search?part=snippet&relatedToVideoId=" +vidID+ "&type=video&maxResults=10&key=AIzaSyD1Nal2lugzvpAwTYh7g4W0tETmrjSlxKY";
+            var relatedAPI = "https://www.googleapis.com/youtube/v3/search?part=snippet&relatedToVideoId=" +vidID+ "&type=video&maxResults=15&key=AIzaSyD1Nal2lugzvpAwTYh7g4W0tETmrjSlxKY";
             $.getJSON(relatedAPI, function(data) {
                 //alert(data.items[0].snippet.title);
                 data.items.forEach(getRelated);
@@ -41,7 +55,7 @@
 
     function getRelated(item, index) {
         //relatedLinks = relatedLinks + "<br/>";
-        relatedLinks = relatedLinks + `<div style="display: flex; flex: none;"><a href=https://www.youtube.com/watch?v=` +item.id.videoId+ `><img width="168" style="flex: 1;" src=` +item.snippet.thumbnails.medium.url+ `></img></a><div #video-title style="font-size: 14px; flex: 1; padding-left: 10px; margin-bottom: 30px;"><div style="font-weight: 500;">` +item.snippet.title+ `</div>` +item.snippet.channelTitle+ `<br/>` +jQuery.timeago(item.snippet.publishedAt)+ `</div></div><br/>`;
+        relatedLinks = relatedLinks + `<div style="display: flex; flex: none;"><a href=https://www.youtube.com/watch?v=` +item.id.videoId+ `><img width="168" style="flex: 1;" src=` +item.snippet.thumbnails.medium.url+ `></img></a><div flex: 1; margin-bottom: 30px;"><div style="font-weight: 500; font-size: 14px; padding-left: 10px;">` +item.snippet.title+ `</div><p style="padding-left: 10px; padding-top: 8px; font-size: 12px; color: #909090;">` +item.snippet.channelTitle+ `<br/>` +jQuery.timeago(item.snippet.publishedAt)+ `</p></div></div><br/>`;
     }
 
     function getUrlVars() {
@@ -51,4 +65,5 @@
         });
         return vars;
     }
+
 })();
